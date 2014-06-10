18af54d6a58eae58dc692250c9db7c2847d82195
Alexander Danilov <snaiffer@gmail.com>
2014-06-10 14:13:25 +0400

    Bugs were fixed

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Intalling..."
+
+# clone:
+  cd ~
+  git clone https://github.com/snaiffer/bash_env.git bash_env
+
+# interactive  
+## a login shell (Ordinary terminal) for one user: 
+  echo "source $PWD/bash_env/bash_env" >> .bash_profile
+## a login shell (Ordinary terminal) for all users:
+  sudo bash -c ' echo "source $PWD/bash_env/bash_env" >> /etc/profile '
+## not a login shell (Terminals in X server) for one user:
+  echo "source $PWD/bash_env/bash_env" >> .bashrc
+
+# non-interactive (shell scripts)
+## a login shell (Ordinary terminal) for one user: 
+  echo "export BASH_ENV=$PWD/bash_env/bash_env" >> .bash_profile
+## a login shell (Ordinary terminal) for all users:
+  sudo bash -c ' echo "export BASH_ENV=$PWD/bash_env/bash_env" >> /etc/profile '
+## not a login shell (Terminals in X server) for one user:
+  echo "export BASH_ENV=$PWD/bash_env/bash_env" >> .bashrc
+
+echo "done."  
